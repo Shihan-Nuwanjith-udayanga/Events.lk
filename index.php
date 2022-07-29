55dc5b514eab47e7441e9083cb4d326e29fdba5c
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-07-29 17:21:08 +0530

    link to normalize.css and index.css stylesheets in index.php file

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="assets/img/logo_tab.jpeg">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/index.css">
     <title>Events.lk</title>
 </head>
 <body>
