40e54ed3aeca7cff0ae39948ff4ecda05304cb5f
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-07-29 16:59:38 +0530

    create index.php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="assets/img/logo_tab.jpeg">
+    <title>Events.lk</title>
+</head>
+<body>
+<header>
+
+</header>
+<main></main>
+<footer></footer>
+</body>
+</html>
