233adfda6f2893e3b3b7311cb46003380f126c81
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-07-30 16:11:15 +0530

    create about us area content

---
+++ assets/php/about_us.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../img/logo_tab.jpeg">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/headerStyle.css">
+    <link rel="stylesheet" href="../css/footerStyle.css">
+    <link rel="stylesheet" href="../css/about_us.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <title>ABOUT US</title>
+</head>
+<body>
+<!--Header Start-->
+<header>
+    <aside>
+        <a href="">
+            <img src="../img/logo.jpeg" alt=".jpeg">
+        </a>
+    </aside>
+    <nav>
+        <ul>
+            <a href="../../index.php">
+                <li><i class="fa-solid fa-house"></i>HOME</li>
+            </a><a href="about_us.php">
+                <li>ABOUT US</li>
+            </a><a href="">
+                <li>EVENTS</li>
+            </a><a href="contact_us.php">
+                <li>CONTACT US</li>
+            </a><a href="">
+                <li>REGISTER</li>
+            </a><a href="">
+                <li><i class="fa-solid fa-lock"></i>LOG IN</li>
+            </a>
+        </ul>
+    </nav>
+</header>
+<!--End header-->
+<main>
+    <h1>ABOUT US</h1>
+    <section>
+        <p>Events.lk is growing to become the Sri Lanka’s leading event information platform.
+            We have group of well-resourced professionals who have more than 10 years of experience in event management
+            and concept developing field consuming their capabilities with the latest technology to meet international
+            standards.
+            searching for upcoming events events.lk is the place to find them by giving our customers the maximum
+            service with a new experience.
+            We're constantly rolling out new features to improve our service with an outstanding selection of
+            tickets.</p>
+        <ul>
+            <li>Movie tickets</li>
+            <li>Bus tickets ,</li>
+            <li>Lottery tickets ,</li>
+            <li>Ticket deals and offers ,</li>
+            <li>Hotel offers and many more to come in near future.</li>
+        </ul>
+        <img src="../img/logo.jpeg" alt="JPEG">
+    </section>
+</main>
+<footer>
+    <section>
+        <div>
+            <i class="fa-solid fa-headset"></i>
+            <h4>For assistance call</h4>
+            <h3>+94 727 300 400 / +94 727 600 700</h3>
+        </div>
+        <div>
+            <i class="fa-solid fa-clock"></i>
+            <h4>Working Hours</h4>
+            <h3>9.00 AM - 5.00 PM</h3>
+        </div>
+        <div>
+            <i class="fa-solid fa-paper-plane"></i>
+            <h4>email us on</h4>
+            <a href=""><h3>support@events.lk</h3></a>
+        </div>
+    </section>
+    <section>
+        <div>
+            <ul>
+                <a href="">
+                    <li>HOME</li>
+                </a><a href="">
+                    <li>ABOUT US</li>
+                </a><a href="">
+                    <li>EVENTS</li>
+                </a><a href="">
+                    <li>CONTACT US</li>
+                </a><a href="">
+                    <li>REGISTER</li>
+                </a><a href="">
+                    <li>LOG IN</li>
+                </a>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <a href="">
+                    <li>FAQ</li>
+                </a><a href="">
+                    <li>Privacy Policy</li>
+                </a><a href="">
+                    <li>Terms & conditions</li>
+                </a><a href="">
+                    <li>Sitemap</li>
+                </a>
+            </ul>
+        </div>
+        <div>
+            <h4>Address</h4>
+            <address>
+                Events.lk <br>
+                449/12, <br>
+                Kalukanda,<br>
+                Thalgasyaya,<br>
+                Akmeemana,<br>
+                Galle, Sri Lanka
+            </address>
+        </div>
+        <div>
+            <h4>Follow us on</h4>
+            <ul>
+                <a href=""><i class="fa-brands fa-linkedin"></i></a>
+                <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a>
+            </ul>
+        </div>
+    </section>
+    <section>
+        <h5>Copyright 2015 © Events.lk All Rights Reserved.</h5>
+    </section>
+</footer>
+</body>
+</html>
