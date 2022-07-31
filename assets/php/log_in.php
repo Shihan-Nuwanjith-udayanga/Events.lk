c15b2cda2cb24706aa49b7685f4b024142d32170
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-07-31 23:18:16 +0530

    create login.php file

---
+++ assets/php/log_in.php
@@ -0,0 +1,15 @@
+<?php require_once('../inc/connection.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log IN</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php //mysqli_close($connection); ?>
