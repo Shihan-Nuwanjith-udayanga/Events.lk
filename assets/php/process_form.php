52798dfcb5df263c1b464300fdf73b468faa3d2d
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-08-03 20:41:50 +0530

    create new process_form sample file

---
+++ assets/php/process_form.php
@@ -0,0 +1,22 @@
+<!--Checking whether the user has pressed the submit button-->
+<pre>
+    <?php
+    if (isset($_POST['submit'])) {
+        print_r($_POST);
+    }
+
+    ?>
+</pre>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Process Form</title>
+</head>
+<body>
+
+</body>
+</html>
